5be3249c8d5869e06d28fc33e4fbf86409dd52cc
Vaishnavi Pandey <70529500+vaishnavipandey-vp@users.noreply.github.com>
2021-06-06 22:16:30 +0530

    Added script.js

---
+++ script.js
@@ -0,0 +1,50 @@
+/*function brac(){
+    alert("hi");
+
+    var o=0;
+    
+    if(c==0){
+        cal.display.value+='(';
+
+    }
+
+
+
+
+
+}
+
+
+############################################################################################################################
+function pos_neg(){
+    
+<h1><span style= "color:black; font-size:80px">C</span> A L C U L A T O <span style="color: black;">R</span></h1>
+    
+
+
+    if(document.getElementsByClassName("btn").value > 0) {
+        document.getElementsByClassName("btn").value = ('-' + document.getElementsByClassName("btn").value);
+    } 
+    else if(document.getElementsByClassName("btn").value < 0) {
+        document.getElementsByClassName("btn").value = ((-1) * document.getElementsByClassName("btn").value);
+    }
+    else {
+        document.getElementsByClassName("btn").value = document.getElementsByClassName("btn").value;
+    }*/
+  
+
+
+/* TO USE IN CSS
+    
+    if(cal.display.value > 0) {
+        cal.display.value = ('-' + cal.display.value)
+    } 
+    else if(cal.display.value < 0) {
+        cal.display.value = ((-1) * cal.display.value)
+    }
+    else {
+        cal.display.value = cal.display.value
+    }
+
+
+*/
